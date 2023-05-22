a7227d72855274b2512d8ad152e86a16ae8060ce
Collins Muchui <muchuicollins56@gmail.com>
2023-05-22 11:20:00 +0300

    the changes

--- script.js
+++ script.js
@@ -8,13 +8,14 @@ function showAllProducts() {
     .then(products => {
       const content = document.getElementById('content');
       content.innerHTML = '<h2>All Products</h2>';
-
+      const productGrid = document.createElement('div');
+      productGrid.className = 'product-grid';
       products.forEach(product => {
-        const productElement = createProductElement(product);
-        content.appendChild(productElement);
+        productGrid.innerHTML += getProductHTML(product);
       });
+      content.appendChild(productGrid);
     })
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
 }
 
 // Function to fetch and display products from a specific category
@@ -24,51 +25,36 @@ function showProductsByCategory(category) {
     .then(products => {
       const content = document.getElementById('content');
       content.innerHTML = `<h2>${capitalizeFirstLetter(category)}</h2>`;
-
+      const productGrid = document.createElement('div');
+      productGrid.className = 'product-grid';
       products.forEach(product => {
-        const productElement = createProductElement(product);
-        content.appendChild(productElement);
+        productGrid.innerHTML += getProductHTML(product);
       });
+      content.appendChild(productGrid);
     })
-    .catch(error => console.log(error));
+    .catch(error => console.error(error));
 }
 
-// Function to display a single product page
+// Function to fetch and display a single product
 function showProduct(productId) {
   fetch(apiURL + 'products/' + productId)
     .then(response => response.json())
     .then(product => {
       const content = document.getElementById('content');
-      content.innerHTML = '';
-
-      const productElement = createProductElement(product);
-      content.appendChild(productElement);
+      content.innerHTML = '<h2>Product Details</h2>';
+      const productHTML = getProductHTML(product);
+      content.innerHTML += productHTML;
     })
-    .catch(error => console.log(error));
-}
-
-// Function to create a product element
-function createProductElement(product) {
-  const productElement = document.createElement('div');
-  productElement.className = 'product';
-  productElement.innerHTML = `
-    <img src="${product.image}" alt="${product.title}">
-    <h3>${product.title}</h3>
-    <p>${product.price}$</p>
-    <button onclick="addToCart(${product.id})">Add to Cart</button>
-  `;
-
-  return productElement;
+    .catch(error => console.error(error));
 }
 
 // Function to add a product to the cart
 function addToCart(productId) {
-  const cartCountElement = document.getElementById('cart-count');
-  let cartCount = parseInt(cartCountElement.textContent);
-  cartCountElement.textContent = ++cartCount;
-
-  // Store the product ID in localStorage or perform other cart operations
-  // based on your implementation requirements
+  // Implement your own logic to handle adding to cart and cart count
+  // based on your requirements and storage method (e.g., localStorage)
+  // Here, we simply increment the cart count displayed in the navigation
+  const cartCount = document.getElementById('cart-count');
+  cartCount.textContent = parseInt(cartCount.textContent) + 1;
 }
 
 // Function to show the cart
@@ -76,6 +62,18 @@ function showCart() {
   // Fetch the cart items from localStorage or perform other cart operations
   // based on your implementation requirements
   // Display the cart items on the page
+}
+
+// Function to generate HTML for a product
+function getProductHTML(product) {
+  return `
+    <div class="product">
+      <img src="${product.image}" alt="${product.title}">
+      <h3>${product.title}</h3>
+      <p>Price: $${product.price}</p>
+      <button onclick="addToCart(${product.id})">Add to Cart</button>
+    </div>
+  `;
 }
 
 // Function to capitalize the first letter of a string
